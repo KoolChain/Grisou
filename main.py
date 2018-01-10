a05875348817dca6b5789434b5df18c97561cea4
Adnn <Adnn@users.noreply.github.com>
2018-01-10 08:09:10 -0800

    Works on the application, to start N workers, and control them from async prompt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from context_utils import AllContexts 
+
 import stream
 
 import argparse
@@ -9,7 +11,7 @@ import json
 
 class NullSystemProcess:
     async def wait(self):
-        await sleep(3600*24*365*10) #TODO replace with an infinit block on a future
+        await sleep(3600*24*365*100) #TODO replace with an infinite block on a future
 
     def terminate(self):
         pass
@@ -27,31 +29,14 @@ class Process:
                                                return_when=asyncio.FIRST_COMPLETED,
             )
             # To raise potential exception
-            result = done.pop().result();
+            result = done.pop().result()
 
             if self.terminate_event.is_set():
+                self.terminate_event.clear()
                 return
 
             print("Process externally closed, relaunch it")
             await self._create_process()
-
-    #async def async_start(self):
-    #    self.process = NullSystemProcess();
-    #    while True:
-    #        wait_list = [self.command_queue.get(), process.wait()]
-    #        done, pending = await asyncio.wait(wait_list,
-    #                                           return_when=asyncio.FIRST_COMPLETED,
-    #        )
-    #        result = done.pop().result();
-    #        if type(result) is Request:
-    #            process.terminate();
-    #            self.command_line = ["xterm", "-e", result.value]
-    #        else:
-    #            print("Process externally closed, relaunch it")
-    #        await _create_process()
-    #
-    #def set_command(self, command):
-    #    self.command_queue = Request(command)
 
     def terminate(self):
         self.terminate_event.set()
@@ -70,48 +55,72 @@ class MiningApp:
 
 
 class Request():
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, workers, currency):
+        self.workers = workers
+        self.currency = currency
 
 
 class Worker:
     def __init__(self, device_id):
         self.device_id = device_id
         self.command_queue = asyncio.Queue()
+        self.process = None
+        asyncio.ensure_future(self._async_mine())
     
-    def set_request(self, request):
-        self.command_queue.put_nowait(Request(request))
+    def __enter__(self):
+        return self
 
-    async def mine(self, mining_app, currency_config):
-        self.process = Process()
-        wait_list = [
-            self.process.async_run(mining_app.get_command(device_id = self.device_id, **currency_config)),
-            self.command_queue.get(),
-        ]
-        done, pending = await asyncio.wait(wait_list, return_when=asyncio.FIRST_COMPLETED)
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.process:
+            self.process.terminate()
+        return False
 
-        result = done.pop().result();
-        if type(result) is Request:
-            self.process.terminate();
-        
-    
+    def mine(self, mining_app, currency_config):
+        self.command_queue.put_nowait((mining_app, currency_config))
+
+    async def _async_mine(self):
+        while True:
+            mining_app, currency_config = await self.command_queue.get()
+
+            if self.process:
+                self.process.terminate()
+                # Await until the currently executing process.async_run completes
+                # (not required, since the soon-to-be replaced self.process is captured in asyncio)
+                await self.process_future
+            else:
+                self.process = Process()
+
+            self.process_future = asyncio.ensure_future(
+                self.process.async_run(mining_app.get_command(device_id = self.device_id,
+                                                              **currency_config)))
+
+
 class CommandPrompt:
     def __init__(self, workers):
         self.workers = workers
 
-    def _get_workers(self, request):
+    def _get_workers(self, read):
         # TODO
-        return [self.workers[0]]
+        return self.workers
 
-    def _get_worker_command(self, read):
+    def _get_currency(self, read):
         # TODO
         return read
 
     async def interact(self):
-        while True:
-            read = await stream.ainput(prompt="Request: ")
-            for worker in self._get_workers(read):
-                worker.set_request(self._get_worker_command(read))
+        read = await stream.ainput(prompt="Request: ")
+        return Request(self._get_workers(read), self._get_currency(read))
+
+
+async def async_interact_loop(prompt, currency_to_app, currency_to_config):
+    while True:
+        request = await prompt.interact()
+        try:
+            for worker in request.workers:
+                worker.mine(currency_to_app[request.currency],
+                            currency_to_config[request.currency])
+        except KeyError as e:
+            print("Currency {} not available.".format(request.currency))
 
 
 if __name__ == "__main__":
@@ -123,21 +132,32 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     #currencies = json.load(open(args.config))
-    #workers = [Worker(i, ) for i in
 
     mining_apps = {
         "XMR": MiningApp("./echoist.sh {pool}"),
         "HUSH": MiningApp("./echoist.sh {wallet}"),
     }
 
-    wk = Worker(0)
-    asyncio.ensure_future(wk.mine(mining_apps["XMR"], {"pool": "lepool", "wallet":"lewallet"}))
+    mining_configs = {
+        "XMR" : {
+            "pool": "leXMRpool",
+            "wallet":"leXMRwallet"
+        },
+        "HUSH" : {
+            "pool": "leHUSHpool",
+            "wallet":"leHUSHwallet"
+        },
+    }
 
-    prompt = CommandPrompt([wk])
-    asyncio.ensure_future(prompt.interact())
-    
+    workers = [Worker(i) for i in range(args.worker_count)]
+    prompt = CommandPrompt(workers)
+
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
-    rc = loop.run_forever()
+
+    with AllContexts(*workers):
+        top_async = async_interact_loop(prompt, mining_apps, mining_configs) 
+        rc = loop.run_until_complete(top_async)
+
     loop.close()
-    
+
