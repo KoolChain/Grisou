cdae0489ea86f4921f3afb7213a1045ea3f500e7
Florian Mounier <florian.mounier@kozea.fr>
2018-01-10 15:05:42 +0100

    Add a multi context context manager.

---
+++ context_utils.py
@@ -0,0 +1,39 @@
+class AllContexts(object):
+    def __init__(self, *contexts):
+        self.contexts = contexts
+
+    def __enter__(self):
+        """Enter all contexts."""
+        return [ctx.__enter__() for ctx in self.contexts]
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        """Exit all contexts and suppress exception
+        if any context wants to suppress it.
+        """
+        return any(
+            ctx.__exit__(exc_type, exc_value, traceback)
+            for ctx in reversed(self.contexts)
+        )
+
+
+if __name__ == '__main__':
+    # Usage:
+    class Context(object):
+        """A mock context manager"""
+
+        def __init__(self, mark):
+            self.mark = mark
+
+        def __enter__(self):
+            print(f'Entering {self.mark}')
+            return self
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            print(f'Exiting {self.mark}')
+            return False
+
+        def __repr__(self):
+            return f'<Mark {self.mark}>'
+
+    with AllContexts(Context('a'), Context('b'), Context('c')) as ctxs:
+        print(f'Current contexts: {ctxs}')
